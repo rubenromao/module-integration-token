df2004b45434b902a4512de08176c7754ed99bd2
rubenromao <rubenromao@gmail.com>
2022-06-17 02:16:22 +0100

    01 - create integration token

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Rubenromao_TestlpwOrderDataAmq',
+    __DIR__
+);
